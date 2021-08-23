1c50608f36e54a9e31caa6b8d2971c31207dd50f
Michael <20vampir11@gmail.com>
2021-08-23 20:53:25 +0300

    working caesar-chipher.py

--- small_projects/loops/caesar-cipher.py
+++ small_projects/loops/caesar-cipher.py
@@ -6,9 +6,27 @@ englishBigLetters = range(ord("A"), ord("Z") + 1)
 text = input(
     "Введите текст с использованием русских или латинских символов (остальные не будут изменены)\n"
 )
-shift = int(input("Насколько нужно сделать сдвиг?"))
+shift = int(input("Насколько нужно сделать сдвиг?\n"))
 
 for char in text:
     code = ord(char)
+    selectedRange = None
+
     if code in russianSmallLetters:
+        selectedRange = russianSmallLetters
+    elif code in russianBigLetters:
+        selectedRange = russianBigLetters
+    elif code in englishSmallLetters:
+        selectedRange = englishSmallLetters
+    elif code in englishBigLetters:
+        selectedRange = englishBigLetters
+
+    if selectedRange != None:
+        shift %= len(selectedRange)
         code += shift
+        if code >= selectedRange.stop:  # selectedRange.stop is after the last letter
+            code -= len(selectedRange)
+        elif code < selectedRange.start:
+            code += len(selectedRange)
+
+    print(chr(code), end="")
