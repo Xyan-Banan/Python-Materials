b1ee8977ebc2f66733f16049b01c6b10cac2fddb
Mike <20vampir11@gmail.com>
2021-09-13 14:57:25 +0300

    updated exceptions

--- 8. exceptions.py
+++ 8. exceptions.py
@@ -1,20 +1,19 @@
-animals = ['cat', 'dog', 'rabbit', 'guinea pig']
+animals = ["cat", "dog", "rabbit", "guinea pig"]
 
 print(f"animals {animals}")
-animals.insert(1,"horse")
-#animals[1] = "horse"
+animals.insert(1, "horse")
 print(f"animals {animals}")
 animals.count("horse")
 
 try:
     print(animals.index("frog"))
-    animals.remove('rabbit')
+    animals.remove("rabbit")
 except ValueError:
     print("some errors")
 
 try:
     print(animals.index("frog"))
-    animals.remove('rabbit')
+    animals.remove("rabbit")
 except ValueError as err:
     print(err)
 
@@ -26,26 +25,28 @@ else:
 
 print("--------------------------")
 
+
 def divide(x, y):
-    try:                                    #пробуем сделать что-то
+    try:  # пробуем сделать что-то
         result = x / y
-    except ZeroDivisionError:               #ожидаем конкретную ошибку
+    except ZeroDivisionError:  # ожидаем конкретную ошибку
         print("division by zero!")
-    else:                                   #выполняем действия, если ничего плохого не случилось
+    else:  # выполняем действия, если ничего плохого не случилось
         print("result is", result)
         return result
-    finally:                                #выполняем обязательные действия, независимо от результата команд
+    finally:  # выполняем обязательные действия, независимо от результата команд
         print("executing finally clause")
         # return 0
 
-print(divide(22,7))
-print(divide(333,106))
-print(divide(3,0))
+
+print(divide(22, 7))
+print(divide(333, 106))
+print(divide(3, 0))
 
 print("--------------------------")
 try:
     print(animals.index("frog"))
-    animals.remove('rabbit')
+    animals.remove("rabbit")
 except (ValueError) as err:
     print("Value error")
     print(err)
@@ -56,9 +57,9 @@ except (Exception) as err:
 print("--------------------------")
 try:
     print(animals.index("frog"))
-    animals.remove('rabbit')
+    animals.remove("rabbit")
     print(animals[100])
-except (ValueError,IndexError) as err:
+except (ValueError, IndexError) as err:
     print("All possible errors come here")
     print(type(err))
     print(err)
